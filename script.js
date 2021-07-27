ec5644f4126ddfdd6b70cef0ec12d16a0e756ea3
Maberry87 <michael123087@gmail.com>
2021-07-26 19:48:54 -0500

    updated script
    First Commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener('load',function() {
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response) {
+        response.json().then(function(json) {
+           
+            let myCrewDiv = document.getElementById('container');
+                for(let i = 0; i < json.length; i++){
+                console.log(json[i]);
+
+            myCrewDiv.innerHTML += `
+            <div class = "astronaut">
+                <div class = "bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+            <ul>
+                <li>${json[i].hoursInSpace}</li>
+                <li>${json[i].active}</li>
+                <li>${json[i].skills}</li>
+                </ul>
+                <img class = "avatar" src = "${json[i].picture}">
+            </div>
+                </div>
+    
+            `
+        }
+
+        });
+    });
+
+});
