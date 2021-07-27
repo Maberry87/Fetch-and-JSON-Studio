3aa5562cabc53aee006bb281c14a733eaaa33504
Maberry87 <michael123087@gmail.com>
2021-07-26 20:19:49 -0500

    updated spacing
    updated spacings

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // TODO: add code here
-window.addEventListener('load',function() {
+window.addEventListener('load',function(){
     this.fetch("https://handlers.education.launchcode.org/static/astronauts.json")
-    .then(function(response) {
-        response.json().then(function(json) {
+    .then(function(response){
+        response.json().then(function(json){
            
             let myCrewDiv = document.getElementById('container');
                 for(let i = 0; i < json.length; i++){
